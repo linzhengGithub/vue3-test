0f399b16827f09d2d97dc05e0a960007f4990210
linwnaglei <947169094@qq.com>
2022-04-02 14:42:05 +0800

    引入element3

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import router from './router/index'
 import store from './store/index'
-createApp(App)
-    .use(store)
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
+
+const app = createApp(App)
+
+app.use(store)
+    .use(Element3)
     .use(router)
     .mount('#app')
